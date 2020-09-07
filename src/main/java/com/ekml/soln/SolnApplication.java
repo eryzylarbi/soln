761d847d4fa8d693260d1bb37e6ddc665c4db777
Erasmus Larbi <ekmlarbi@gmail.com>
2020-09-07 16:12:52 -0400

    initial commit - landing page

---
+++ src/main/java/com/ekml/soln/SolnApplication.java
@@ -0,0 +1,13 @@
+package com.ekml.soln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolnApplication.class, args);
+	}
+
+}
